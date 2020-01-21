3d406ff36355323d37293e5ecd790cdc7ee10382
eneto <ernesto.jara06@gmail.com>
2020-01-21 00:20:44 -0600

    start

---
+++ eneto-ui/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+ReactDom.render(
+    <div>
+        <button> troll</button>
+    </div>,
+    document.getElementById("root")
+);
